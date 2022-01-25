817c3a393a68d644b47c5b1a77e76acff3c184dc
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-01-25 18:45:38 +0100

    definitive php data structure

--- index.php
+++ index.php
@@ -16,30 +16,19 @@
 
 </head>
 <body>
-    <div class="container">
-        <span>
-            <h4>
-                Come state implementando la recente decisione della Corte di giustizia dell'Unione 
-                europea (CGUE) relativa al diritto all'oblio?
-            </h4>
-            <br>
-            <p>
-                La recente <a href="">decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze 
-                per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto 
-                di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di 
-                ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati 
-                devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
-            </p>
-            <br>
-            <p>
+    <?php 
+        $faqs = [
+            [
+                'question' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+
+                'answer' => "La recente <a href=\"#\">decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.<br><p>
                 Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente 
                 per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola 
                 richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati 
                 personali ed il diritto di tutti di conoscere e distribuire le informazioni. 
-            </p>
-            <br>
-            <p>
-                Per presentare una richiesta di rimozione, compila questo <a href="">modulo web</a>. Riceverai una risposta automatica che conferma 
+                </p>
+                <br><p>
+                Per presentare una richiesta di rimozione, compila questo <a href=\"#\">modulo web</a>. Riceverai una risposta automatica che conferma 
                 la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di 
                 tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati 
                 includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a 
@@ -48,23 +37,38 @@
                 eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione 
                 giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti 
                 all'Autorità garante per la protezione dei dati personali nel tuo paese.
-            </p>
-            <br>
-            <p>
-                Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per 
-                perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di 
-                ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione 
-                della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
-            </p>
-            <br>
-            <p>
-                Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati 
-                modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei 
-                quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.
-            </p>
-        </span>
+                </p>
+                <br>
+                <p>
+                    Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per 
+                    perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di 
+                    ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione 
+                    della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
+                </p>
+                <br>
+                <p>
+                    Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati 
+                    modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei 
+                    quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.
+                </p>"
+            ],
+            
+            
+        ];
+    ?>
+    <div class="container">
+        
+        
 
-        <span>
+        <?php foreach($faqs as $faq){ ?> 
+            <span>
+                <h4><?php echo $faq['question']; ?></h4>
+                <p><?php echo $faq['answer']; ?></p>
+            </span>
+        <?php }?>
+        
+
+        <!-- <span>
             <h4>
                 Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?
             </h4>
@@ -190,7 +194,7 @@
                 informazioni relative all' esatte parole chiave che hanno determinato il clic su un 
                 annuncio.
             </p>
-        </span>
+        </span> -->
 
     </div>
 </body>
