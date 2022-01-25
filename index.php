c18e86eaaa36012035e5e58e5816bebaac359899
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-01-25 17:39:55 +0100

    Q&A html css structure elements  completed

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
             </h4>
             <br>
             <p>
-                La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze 
+                La recente <a href="">decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze 
                 per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto 
                 di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di 
                 ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati 
@@ -39,7 +39,7 @@
             </p>
             <br>
             <p>
-                Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma 
+                Per presentare una richiesta di rimozione, compila questo <a href="">modulo web</a>. Riceverai una risposta automatica che conferma 
                 la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di 
                 tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati 
                 includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a 
@@ -86,11 +86,11 @@
             <br>
             <p>
                 Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, 
-                nel Centro Google per la sicurezza online.
+                nel <a href="">Centro Google per la sicurezza online</a>.
             </p>
             <br>
             <p>
-                Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a 
+                <a href="">Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a 
                 te il controllo.
             </p>
         </span>
@@ -128,6 +128,25 @@
                 Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che 
                 li offre o dal paese a cui è associato il tuo account.
             </p>
+            <br>
+            <h5>Stabilire il paese associato al tuo account</h5>
+            <p>
+                Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda 
+                gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i 
+                servizi in cui hai trascorso più tempo nell'ultimo anno.
+            </p>
+            <br>
+            <p>
+                I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro 
+                paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
+            </p>
+            <br>
+            <p>
+                Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la 
+                differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale 
+                (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href="">Contattaci</a> se 
+                ritieni che il paese associato al tuo account sia sbagliato.
+            </p>
         </span>
 
         <span>
@@ -139,15 +158,14 @@
                 I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili 
                 sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti 
                 web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i 
-                contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il 
-                webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una 
+                contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href="">contattare il 
+                webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una 
                 modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, 
                 desideri richiedere la rimozione di determinate informazioni su di te visualizzate 
-                nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno 
+                nei risultati di ricerca di Google, <a href="">fai clic qui</a>. Una volta che i contenuti saranno 
                 stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non 
                 verranno più visualizzate nei risultati di ricerca di Google. In caso di una 
-                richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di 
-                assistenza per avere ulteriori informazioni.
+                richiesta di rimozione urgente, è inoltre possibile <a href="">visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>.
             </p>
         </span>
 
@@ -160,13 +178,13 @@
             <p>
                 In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo 
                 browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo 
-                Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. 
+                Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href="">URL referrer</a>. 
                 Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di 
                 ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata 
                 di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati 
                 come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad 
                 esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca 
-                SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL 
+                SSL sono disponibili <a href="">qui</a>. Le query di ricerca o le informazioni contenute nell'URL 
                 referrer potrebbero essere disponibili mediante Google Analytics o un'API 
                 (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere 
                 informazioni relative all' esatte parole chiave che hanno determinato il clic su un 
